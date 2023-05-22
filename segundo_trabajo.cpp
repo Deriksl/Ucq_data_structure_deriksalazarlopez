a981dec5a70bdffc1fdde4d2fb5793a9be515df1
Deriksl <133131434+Deriksl@users.noreply.github.com>
2023-05-22 14:44:25 -0600

    Add files via upload

---
+++ segundo_trabajo.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main()
+{
+ int myDeclaration = 5;
+
+int i = 5;
+
+int j;
+
+std::cout << i << '\n';
+
+int myArray[5] = { 0, 11, 22, 337, 45 };
+for (int i = 0; i < 5; i++)
+{
+  std::cout << myArray[i] << '\n';
+}
+
+ std::cout << &myDeclaration << '\n';
+
+int* myAdress = &myDeclaration;
+
+
+std::cout << "Direcciones de memoria a que apunta MyAddress:" << myAddress << '\n';
+std::cout << "Valor de la variable a la que apunta MyAddress:" << *myAddress << '\n';
+
+std::cout << &myAdress << '\n'
+
+int** myAddressAddress = &myAdress;
+
+myArray[] ~ *
+
+myTable[][] ~ []* ~ **
+
+int* myArrayPtr = myArray[0];
+
+for (int i = 0; i < 5; i++)
+{
+  std::cout << myArray[i] << '\n';
+}
+
+}
+
